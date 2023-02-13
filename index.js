f2c903ae9a7a4b8531d74c74d9e64a110011b618
krishna8464 <yendavamshikrishna123@gmail.com>
2023-02-14 00:34:46 +0530

    done

--- index.js
+++ index.js
@@ -1,107 +1,110 @@
-//  var mybutton = document.getElementById("myBtn");
+ // ----------------------------------button to take you top starts -------------------------------------//
+ var mybutton = document.getElementById("myBtn");
 
 // // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () {
-//   scrollFunction();
-// };
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
+ function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 // // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
-// }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
-// var string = "VAMSHI KRISHNA";
-// var str = string.split("");
-// var el = document.getElementById("str");
+// ---------------------------------------button to take you top end --------------------------------------//
 
-// (function animate() {
-//   str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
-//   var running = setTimeout(animate, 90);
-// })();
+var string = "VAMSHI KRISHNA";
+var str = string.split("");
+var el = document.getElementById("str");
 
-// // code sleep rep
-// // get the element
-// const text1 = document.querySelector(".typing-text");
+(function animate() {
+  str.length > 0 ? (el.innerHTML += str.shift()) : clearTimeout(running);
+  var running = setTimeout(animate, 90);
+})();
 
-// // make a words array
-// const words1 = ["eat.", "sleep.", "code.", "repeat."];
+// code sleep rep
+// get the element
+const text1 = document.querySelector(".typing-text");
 
-// // start typing effect
-// setTyper(text1, words1);
+// make a words array
+const words1 = ["eat.", "sleep.", "code.", "repeat."];
 
-// const text = document.querySelector(".typing-dev");
+// start typing effect
+setTyper(text1, words1);
 
-// // make a words array
-// const words = [
+const text = document.querySelector(".typing-dev");
+
+// make a words array
+const words = [
  
-//   "Node Backend Developer..."
-// ];
+  "Node Backend Developer..."
+];
 
-// // start typing effect
-// setTyper(text, words);
+// start typing effect
+setTyper(text, words);
 
-// function setTyper(element, words) {
-//   const LETTER_TYPE_DELAY = 75;
-//   const WORD_STAY_DELAY = 2000;
+function setTyper(element, words) {
+  const LETTER_TYPE_DELAY = 75;
+  const WORD_STAY_DELAY = 2000;
 
-//   const DIRECTION_FORWARDS = 0;
-//   const DIRECTION_BACKWARDS = 1;
+  const DIRECTION_FORWARDS = 0;
+  const DIRECTION_BACKWARDS = 1;
 
-//   var direction = DIRECTION_FORWARDS;
-//   var wordIndex = 0;
-//   var letterIndex = 0;
+  var direction = DIRECTION_FORWARDS;
+  var wordIndex = 0;
+  var letterIndex = 0;
 
-//   var wordTypeInterval;
+  var wordTypeInterval;
 
-//   startTyping();
+  startTyping();
 
-//   function startTyping() {
-//     wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
-//   }
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+  }
 
-//   function typeLetter() {
-//     const word = words[wordIndex];
+  function typeLetter() {
+    const word = words[wordIndex];
 
-//     if (direction == DIRECTION_FORWARDS) {
-//       letterIndex++;
+    if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
 
-//       if (letterIndex == word.length) {
-//         direction = DIRECTION_BACKWARDS;
-//         clearInterval(wordTypeInterval);
-//         setTimeout(startTyping, WORD_STAY_DELAY);
-//       }
-//     } else if (direction == DIRECTION_BACKWARDS) {
-//       letterIndex--;
+      if (letterIndex == word.length) {
+        direction = DIRECTION_BACKWARDS;
+        clearInterval(wordTypeInterval);
+        setTimeout(startTyping, WORD_STAY_DELAY);
+      }
+    } else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
 
-//       if (letterIndex == 0) {
-//         nextWord();
-//       }
-//     }
+      if (letterIndex == 0) {
+        nextWord();
+      }
+    }
 
-//     const textToType = word.substring(0, letterIndex);
+    const textToType = word.substring(0, letterIndex);
 
-//     element.textContent = textToType;
-//   }
+    element.textContent = textToType;
+  }
 
-//   function nextWord() {
-//     letterIndex = 0;
-//     direction = DIRECTION_FORWARDS;
-//     wordIndex++;
+  function nextWord() {
+    letterIndex = 0;
+    direction = DIRECTION_FORWARDS;
+    wordIndex++;
 
-//     if (wordIndex == words.length) {
-//       wordIndex = 0;
-//     }
-//   }
-// }
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+  }
+}
 
 
 // // main project bus 
