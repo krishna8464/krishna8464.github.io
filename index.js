c7a645cd734cb702cb5f3876a6deea20e0a4bb66
krishna8464 <yendavamshikrishna123@gmail.com>
2023-02-13 23:25:56 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// var mybutton = document.getElementById("myBtn");
+//  var mybutton = document.getElementById("myBtn");
 
 // // When the user scrolls down 20px from the top of the document, show the button
 // window.onscroll = function () {
